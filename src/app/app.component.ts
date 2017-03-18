ae8b115b75fe04f1fb88fbceae72f12d1b4572b5
Vitor <metroid.p2p@gmail.com>
2017-03-18 13:19:49 -0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  zom: number = 1;
+  lat: number = -22.255374;
+  lng: number = -45.696035;
+
+  markers: marker[] = [
+    {
+      name: 'Company One',
+      lat: -22.262166,
+      lng: -45.702880,
+      draggable: true
+    },
+    {
+      name: 'Company Two',
+      lat: -22.256746,
+      lng: -45.694612,
+      draggable: true
+    },
+    {
+      name: 'Company Three',
+      lat: -22.253310,
+      lng:  -45.700481,
+      draggable: true
+    }
+  ];
+
+  constructor() {
+
+  }
+}
+
+
+interface marker {
+  name:string;
+  lat: number;
+  lng: number;
+  draggable: boolean;
 }
