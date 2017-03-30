3c4ae300a67303248d3cb66feaca8ff8cf82bede
Vitor <metroid.p2p@gmail.com>
2017-03-30 20:59:15 -0300

    finnaly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,6 +55,8 @@ export class AppComponent {
 
     let newLat = $event.coords.lat;
     let newLng = $event.coords.Lng;  
+
+    this.markersService.updateMarker(updMarker, newLat, newLng);
   }
 
   addMarker() {
@@ -76,6 +78,17 @@ export class AppComponent {
 
     this.markers.push(newMarker);
     this.markersService.addMarker(newMarker);
+  }
+
+  removeMarker(marker) {
+    console.log(marker);
+    for (var i = 0; i < this.markers.length; i++) {
+      if (marker.lat == this.markers[i].lat && marker.lng == this.markers[i].lng) {
+        this.markers.splice(i, 1);
+      }
+      
+    }
+    
   }
 }
 
--- src/app/services/marker.service.ts
+++ src/app/services/marker.service.ts
@@ -20,7 +20,19 @@ export class MarkerService extends Init {
     markers.push(newMarker);
     localStorage.setItem('markers', JSON.stringify(markers));
     console.log(localStorage.getItem('markers'));
-    
+  }
+
+  updateMarker(marker, newLat, newLng) {
+    var markers = JSON.parse(localStorage.getItem('markers'));
+
+    for(let i = 0; i < markers.length; i++) {
+      if(marker.lat == markers[i].lat && marker.lng == markers[i].lng) {
+        markers[i].lat = newLat;
+        markers[i].lng = newLng;
+      }
+    } 
+    localStorage.setItem('markers', JSON.stringify(markers));
+    console.log('updateMarker');
     
   }
 
